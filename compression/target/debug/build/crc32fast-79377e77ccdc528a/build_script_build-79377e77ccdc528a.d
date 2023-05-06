4b26c62957022fa41e41d18edd0396efe516df81
sscodez <ssameershah1200@gmail.com>
2023-05-06 20:53:21 +0500

    add

---
+++ compression/target/debug/build/crc32fast-79377e77ccdc528a/build_script_build-79377e77ccdc528a.d
@@ -0,0 +1,5 @@
+H:\Rust-Projects\compression\target\debug\build\crc32fast-79377e77ccdc528a\build_script_build-79377e77ccdc528a.exe: C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\crc32fast-1.3.2\build.rs
+
+H:\Rust-Projects\compression\target\debug\build\crc32fast-79377e77ccdc528a\build_script_build-79377e77ccdc528a.d: C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\crc32fast-1.3.2\build.rs
+
+C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\crc32fast-1.3.2\build.rs:
---
+++ compression/target/debug/deps/adler-9a9038f1a29773c3.d
@@ -0,0 +1,8 @@
+H:\Rust-Projects\compression\target\debug\deps\adler-9a9038f1a29773c3.rmeta: C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\adler-1.0.2\src\lib.rs C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\adler-1.0.2\src\algo.rs
+
+H:\Rust-Projects\compression\target\debug\deps\libadler-9a9038f1a29773c3.rlib: C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\adler-1.0.2\src\lib.rs C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\adler-1.0.2\src\algo.rs
+
+H:\Rust-Projects\compression\target\debug\deps\adler-9a9038f1a29773c3.d: C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\adler-1.0.2\src\lib.rs C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\adler-1.0.2\src\algo.rs
+
+C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\adler-1.0.2\src\lib.rs:
+C:\Users\CORECOM\.cargo\registry\src\github.com-1ecc6299db9ec823\adler-1.0.2\src\algo.rs:
